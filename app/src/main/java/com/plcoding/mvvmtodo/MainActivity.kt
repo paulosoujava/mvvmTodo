d0492f2f3c3e0c01b4f9a50a8ccc0bfb93598485
paulosoujava <paulosoujava@gmail.com>
2022-02-22 17:24:47 -0300

    first commit

---
+++ app/src/main/java/com/plcoding/mvvmtodo/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.plcoding.mvvmtodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.plcoding.mvvmtodo.ui.add_edit_todo.AddEditTodoScreen
+import com.plcoding.mvvmtodo.ui.theme.DictionaryTheme
+import com.plcoding.mvvmtodo.ui.todo_list.TodoListScreen
+import com.plcoding.mvvmtodo.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DictionaryTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/plcoding/mvvmtodo/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.plcoding.mvvmtodo.di
+
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.mvvmtodo.data.TodoDatabase
+import com.plcoding.mvvmtodo.data.TodoRepository
+import com.plcoding.mvvmtodo.data.TodoRepositoryImpl
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
+}
